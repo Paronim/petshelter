a461beea080f86e46770bf65ebf0ba03169f60ce
Nikita Androsenko <varzaid@gmail.com>
2023-03-14 13:48:49 +0300

    Настроен вывод тестовых постов из Hasura в IndexPage

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,51 +1,39 @@
-<!-- <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
-</template>
-
-<script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'IndexPage'
-})
-</script> -->
-
 <template>
   <q-page class="row items-center justify-evenly">
-    <div v-if="loading">Loading...</div>
-    <div v-else-if="error">Error: {{ error.message }}</div>
-    <div v-else-if="result && result.posts">
-      <div>id: {{ result.posts.id }}</div>
-      <div>title: {{ result.posts.title }}</div>
-    </div>
+    <q-list>
+      <q-item-label header> Post List </q-item-label>
+
+      <q-item v-for="post in posts" :key="post.id">
+        <q-item-section class="my-box q-hoverable">
+          <q-item-label>
+            {{ post.title }}
+          </q-item-label>
+          <q-item-label caption>ID: {{ post.id }}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
 <script>
 import { useQuery } from "@vue/apollo-composable";
 import gql from "graphql-tag";
-import { defineComponent } from "vue";
+import { defineComponent, computed } from "vue";
 
 export default defineComponent({
+  name: "IndexPage",
   setup() {
     const { result, loading, error } = useQuery(gql`
-      query MyQuery {
+      query {
         posts {
           id
           title
         }
       }
     `);
-    console.log(result);
-    console.log(result.posts);
+    const posts = computed(() => result.value?.posts ?? null);
 
-    return { /* your other items, */ result, loading, error };
+    return { posts };
   },
 });
 </script>
