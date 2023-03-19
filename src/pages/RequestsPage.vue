e2a02e11b935c4ca39cd73e5928b75711214d4b0
Andrew Nasibullin <duhich02@yandex.ru>
2023-03-19 19:53:02 +0300

    pre merging

--- src/pages/RequestsPage.vue
+++ src/pages/RequestsPage.vue
@@ -2,13 +2,13 @@
   <div class="q-pa-md q-gutter-sm">
     <q-btn label="Click me" color="primary" @click="prompt = true" />
     <q-dialog v-model="prompt" persistent >
-      <q-card style="min-width: 350px;background: rgba(40, 40, 40, 0.85);border-radius: 25px;">
+      <q-card style="min-width: 450px;border-radius: 25px;">
         <q-card-section>
           <div class="text-h6" style="color: #fefefe">ОСТАВИТЬ ЗАЯВКУ</div>
         </q-card-section>
-        <q-card-section class="q-pt-none" style="color: #fefefe">
+        <q-card-section class="q-pt-none" style="color: #fefefe; ">
           <q-input
-            style="border: 4px solid #EF7540;border-radius: 25px;padding: 0; margin: 0 0 10px;color: #fefefe"
+            standout="bg-primary text-white"
             filled
             v-model="req.name"
             label="Кличка питомца"
@@ -17,7 +17,7 @@
           />
           <div style="display: flex; justify-content: space-between;margin: 0 0 10px">
             <q-input
-              style="border: 4px solid #EF7540;border-radius: 25px;padding: 0;color: #fefefe"
+              standout="bg-primary text-white"
               filled
               v-model="req.age"
               label="возраст"
@@ -25,7 +25,7 @@
               :rules="[ val => val && val.length > 0 || 'Please type something']"
             />
             <q-input
-              style="border: 4px solid #EF7540;border-radius: 25px;margin: 0 10px 0;padding: 0;color: #fefefe"
+              standout="bg-primary text-white"
               filled
               v-model="req.sex"
               label="пол"
@@ -34,7 +34,7 @@
             />
           </div>
           <q-input
-            style="margin: 0 0 10px;border: 4px solid #EF7540;border-radius: 25px;padding: 0;color: #fefefe"
+            standout="bg-primary text-white"
             filled
             v-model="req.FIO"
             label="ФИО"
@@ -42,16 +42,15 @@
             :rules="[ val => val && val.length > 0 || 'Please type something']"
           />
           <q-input
-            style="margin: 0 0 10px;border: 4px solid #EF7540;border-radius: 25px;padding: 0;color: #fefefe"
+            standout="bg-primary text-white"
             filled
             v-model="req.phone"
             label="тел."
             lazy-rules
             :rules="[ val => val && val.length > 0 || 'Please type something']"
           />
-
           <q-input
-            style="margin: 0 0 10px;border: 4px solid #EF7540;border-radius: 25px;padding: 0;color: #fefefe"
+            standout="bg-primary text-white"
             filled
             v-model="req.email"
             label="почта"
@@ -62,7 +61,7 @@
           />
         </q-card-section>
         <q-card-actions align="right" class="text-primary">
-          <q-btn style="background: #EF7540;border-radius: 25px;" flat label="Отмена" v-close-popup />
+          <q-btn style="background: #EF7540; color:white;border-radius: 25px;" flat label="Отмена" v-close-popup />
           <q-btn style="background: #EF7540;border-radius: 25px;" flat label="Забрать домой" @click="onSubmit" v-close-popup />
         </q-card-actions>
       </q-card>
@@ -122,37 +121,10 @@ import store from 'vuex';
     accept,
     methods:{},
     computed: {},
-
-//   onSubmit () {
-//   if (accept.value !== true) {
-//   $q.notify({
-//   color: 'red-5',
-//   textColor: 'white',
-//   icon: 'warning',
-//   message: 'You need to accept the license and terms first'
-// })
-// }
-//   else {
-//   $q.notify({
-//   color: 'green-4',
-//   textColor: 'white',
-//   icon: 'cloud_done',
-//   message: 'Submitted'
-// })
-// }
-// },
-
-  onReset () {
-  name.value = null
-  age.value = null
-  accept.value = false
-}
 }
 }
 }
 </script>
 <style sccs>
-*{
-  color: #fefefe;
-}
+
 </style>
