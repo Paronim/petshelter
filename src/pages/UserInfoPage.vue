ecad8a10fae0c98fd070fa6691e4e15edaa4bf02
Andrew Nasibullin <duhich02@yandex.ru>
2023-03-20 23:51:56 +0300

    Стилизовал панель заявок

--- src/pages/UserInfoPage.vue
+++ src/pages/UserInfoPage.vue
@@ -1,27 +1,34 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <q-list>
-      <q-item-label header> Requests List </q-item-label>
-      <q-item v-for="req in POSTS" :key="req.id">
-        <q-item-section class="my-box q-hoverable">
-          <q-item-label style="color: white">
-            <p
-              style="
-                border: 1px solid #fefefe;
-                border-radius: 10px;
-                padding: 5px;
-              "
-            >
-              {{ req.FIO }} {{ req.phone }} {{ req.email }} requests on getting
-              {{ req.name }} {{ req.age }} {{ req.sex }}
-            </p>
-          </q-item-label>
-        </q-item-section>
-      </q-item>
-    </q-list>
-  </q-page>
+  <div class="q-pa-md">
+    <q-card class="my-card">
+      <q-card-section>
+        <div class="text-h6">Заявки</div>
+      </q-card-section>
+      <q-markup-table>
+        <thead>
+        <tr>
+          <th class="text-left">ФИО покупателя</th>
+          <th class="text-right">Номер тефефона</th>
+          <th class="text-right">Электронная почта</th>
+          <th class="text-right">Кличка зверюшки</th>
+          <th class="text-right">Возраст</th>
+          <th class="text-right">Пол</th>
+        </tr>
+        </thead>
+        <tbody  v-for="req in POSTS" :key="req.id">
+        <tr>
+          <td class="text-left">{{ req.FIO }}</td>
+          <td class="text-right">{{ req.phone }}</td>
+          <td class="text-right">{{ req.email }}</td>
+          <td class="text-right">{{ req.name }}</td>
+          <td class="text-right">{{ req.age }}</td>
+          <td class="text-right">{{ req.sex }}</td>
+        </tr>
+        </tbody>
+      </q-markup-table>
+    </q-card>
+  </div>
 </template>
-
 <script>
 import { mapActions, mapGetters } from "vuex";
 import { defineComponent } from "vue";
