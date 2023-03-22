ffb1a6773d74d39eb0e107b08849f07bbc80f446
Nikita Androsenko <varzaid@gmail.com>
2023-03-22 17:54:17 +0300

    Рабочий чат

--- src/components/MiniChat.vue
+++ src/components/MiniChat.vue
@@ -4,19 +4,18 @@
       style="width: 100%; max-width: 400px"
       class="q-mt-xl q-pa-md bg-white rounded-borders"
     >
-      <div class="q-mb-md row items-start justify-evenly">
-        <q-btn @click="connectChat">Вход</q-btn>
-        <q-btn @click="disconnectChat">Выход</q-btn>
-      </div>
-      <q-list v-for="message in messages">
-        <q-chat-message sent text-color="white" bg-color="primary">
-          <div>{{ message.title }}</div>
-        </q-chat-message>
+      <q-list v-for="message in messages" :key="message.id">
+        <q-chat-message
+          bg-color="primary"
+          text-color="white"
+          :name="message.first_name"
+          :text="[message.text]"
+          :sent="checkSent(message)"
+        />
       </q-list>
-
       <div class="q-mt-md row items-center justify-end no-wrap">
         <q-input
-          v-model="newMessage"
+          v-model="message"
           class="full-width"
           outlined
           label="Сообщение"
@@ -37,54 +36,50 @@
 <script setup>
 import { onMounted, ref } from "vue";
 import { Stomp } from "@stomp/stompjs";
-import { v4 as uuidv4 } from "uuid";
-import Clerk from "@clerk/clerk-js";
 
-// const userId = window.Clerk.user.getUser();
-// console.log(window.Clerk?.user.id);
 const messages = ref([]);
-const newMessage = ref("");
-const receivedMessage = ref("");
+const message = ref("");
 const url = "ws://localhost:15674/ws";
 const client = Stomp.client(url);
+const user = window.Clerk?.user;
+console.log(user);
+console.log(window);
 const headers = {
-  login: "guest",
-  passcode: "guest",
+  login: "chat_user",
+  passcode: "chat_user",
 };
 
-const connectCallback = function () {
-  client.subscribe("/queue/chat", messageCallback);
+const checkSent = (message) => {
+  return message.user_id === user.id;
 };
-const errorCallback = function (error) {
-  console.log(error.headers.message);
-};
-const messageCallback = function (message) {
-  // called when the client receives a STOMP message from the server
-  if (message.body) {
-    // alert("Получил сообщение: " + message.body);
-    receivedMessage.value = message.body;
-    messages.value.push({ title: receivedMessage.value });
-  } else {
-    alert("Пришло пустое сообщение");
-  }
-};
-// client.connect(headers, connectCallback, errorCallback);
 
-const connectChat = () => {
-  client.connect(headers, connectCallback, errorCallback);
-};
-const disconnectChat = () => {
-  client.disconnect(function () {
-    console.log("До свидания");
-  });
-};
 const sendMessage = () => {
-  client.send("/queue/chat", {}, newMessage.value);
-  messages.value.push({ title: newMessage.value });
-  newMessage.value = "";
+  if (message.value.trim()) {
+    const messageData = {
+      user_id: user.id,
+      first_name: user.firstName,
+      text: message.value,
+    };
+    // messages.value.push(messageData);
+    client.send("/exchange/chat", {}, JSON.stringify(messageData));
+  } else {
+    alert("Пустые сообщения лучше не отправлять");
+  }
+  message.value = "";
 };
-// onMounted(() => {
-//   console.log("onMounted success");
-// });
+onMounted(() => {
+  const onConnected = function () {
+    client.subscribe("/exchange/chat", onMessageReceived);
+  };
+  const onError = function (error) {
+    console.log(error.headers.message);
+  };
+  const onMessageReceived = function (message) {
+    if (message.body) {
+      messages.value.push(JSON.parse(message.body));
+    }
+  };
+  client.connect(headers, onConnected, onError);
+});
 </script>
 <style lang="scss"></style>
